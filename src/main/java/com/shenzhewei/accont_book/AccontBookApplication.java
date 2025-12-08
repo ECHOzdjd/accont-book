16d8ef41f55744213d3d4bb2914fe0f2c351a0f1
Ryan Shen <111244647+Ryan041001@users.noreply.github.com>
2025-12-08 09:19:46 +0800

    feat: initialize Spring Boot project with Maven Wrapper and standard file structure

---
+++ src/main/java/com/shenzhewei/accont_book/AccontBookApplication.java
@@ -0,0 +1,13 @@
+package com.shenzhewei.accont_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccontBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccontBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shenzhewei/accont_book/AccontBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenzhewei.accont_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccontBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
